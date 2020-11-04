93df7406c33a76c7a05bc820e2660bed3082e1c1
Kostas <kostaskoutsintas@gmail.com>
2020-11-04 16:21:18 +0200

    simple code

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
