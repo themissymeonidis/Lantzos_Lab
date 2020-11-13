4990484eaf7379018988586fe43d949640225eb3
Themistoklis <themissymeonidis@yahoo.gr>
2020-11-14 00:11:34 +0200

    Merge branch 'master' of https://github.com/themissymeonidis/Lantzos_Lab into under_Kati
     Conflicts:
    	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,16 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a;
-        int b;
-        int c;
-        c = (textView) findViewById(R.id.textView);
-        Button btn0;
-        btn0 = (Button) findViewById(R.id.btnone0) ;
-        btn0.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                c = 0;
-            }
-        });
+
     }
 }
