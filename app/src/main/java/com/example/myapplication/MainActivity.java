8e252e8647f78e84fbbe345c2222ecc1578950d3
Themistoklis <themissymeonidis@yahoo.gr>
2020-11-17 22:00:50 +0200

    Merge branch 'master' of https://github.com/themissymeonidis/Lantzos_Lab into under_Kati
     Conflicts:
    	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,10 +8,10 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
     }
 }
