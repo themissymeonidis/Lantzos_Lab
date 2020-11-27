4b1dee2a2713f41bb1baa8616948b3ebc497f66c
Themistoklis <themissymeonidis@yahoo.gr>
2020-11-27 21:15:33 +0200

    Merge branch 'master' of https://github.com/themissymeonidis/Lantzos_Lab into under_Kati
     Conflicts:
    	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,49 +1,47 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-        import android.os.Bundle;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-        DatabaseHelper myDb;
         @Override
         public void onCreate(Bundle savedInstanceState){
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-            myDb = new DatabaseHelper(this);
 
-        Button userlogin = (Button) findViewById(R.id.button);
+        Button userlogin = (Button) findViewById(R.id.button7);
 
-        userlogin.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                setContentView(R.layout.user_signin);
-            }
-        });
+                  userlogin.setOnClickListener(new View.OnClickListener() {
+              public void onClick(View v) {
+                  setContentView(R.layout.user_signin);
+                }
+                  });
 
-        Button adminlogin = (Button) findViewById(R.id.button3);
+              Button adminlogin = (Button) findViewById(R.id.button14);
 
-        adminlogin.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                setContentView(R.layout.admin_signin);
-            }
-        });
+               adminlogin.setOnClickListener(new View.OnClickListener() {
+                  public void onClick(View v) {
+                      setContentView(R.layout.admin_signin);
+                   }
+               });
 
-        Button usersignup = (Button) findViewById(R.id.button4);
+            //    Button usersignup = (Button) findViewById(R.id.button4);
+//
+            //    usersignup.setOnClickListener(new View.OnClickListener() {
+            //       public void onClick(View v) {
+                //          setContentView(R.layout.user_signup);
+            //      }
+            //     });
 
-        usersignup.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                setContentView(R.layout.user_signup);
-            }
-        });
+            //     Button adminsignup = (Button) findViewById(R.id.button6);
 
-        Button adminsignup = (Button) findViewById(R.id.button6);
-
-        adminsignup.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                setContentView(R.layout.admin_signup);
-            }
-        });
+            //     adminsignup.setOnClickListener(new View.OnClickListener() {
+            //    public void onClick(View v) {
+            //        setContentView(R.layout.admin_signup);
+            //      }
+            //   });
+            //
     }
 }
