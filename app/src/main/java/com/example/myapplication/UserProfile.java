650b37117b0423ea65c9b9181de1da6e7368841b
kyriakosth <pites643@gmail.com>
2020-12-11 20:35:47 +0200

    userprofedit

--- app/src/main/java/com/example/myapplication/UserProfile.java
+++ app/src/main/java/com/example/myapplication/UserProfile.java
@@ -1,27 +1,18 @@
 package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.Source;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -36,6 +27,17 @@ public class UserProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.userprofile);
+
+
+        Button info_update = (Button) findViewById(R.id.info_update);
+
+        info_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),UserProfileEdit.class));
+            }
+        });
+
 
         getname = findViewById(R.id.get_name);
         getaddress = findViewById(R.id.get_address);
