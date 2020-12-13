cea8afa993ad2f425ae67edb4468f38632a60134
Toulis <xxthemissymeonidisxx@gmail.com>
2020-12-13 22:44:30 +0200

    Merge branch 'master' of https://github.com/themissymeonidis/Lantzos_Lab into under_Kati
     Conflicts:
    	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/myapplication/UserProfile.java
+++ app/src/main/java/com/example/myapplication/UserProfile.java
@@ -3,16 +3,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -30,7 +35,7 @@ public class UserProfile extends AppCompatActivity {
         setContentView(R.layout.userprofile);
 
 
-        Button info_update = (Button) findViewById(R.id.info_update);
+        Button info_update = (Button) findViewById(R.id.button5);
 
         info_update.setOnClickListener(new View.OnClickListener() {
             @Override
