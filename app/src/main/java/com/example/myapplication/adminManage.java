bd0a23c94182d9c4806e15916edce5d46b978a73
stasino <stasino9966@gmail.com>
2020-12-15 22:11:34 +0200

    maybe fixedd

--- app/src/main/java/com/example/myapplication/adminManage.java
+++ app/src/main/java/com/example/myapplication/adminManage.java
@@ -54,7 +54,7 @@ public class adminManage extends AppCompatActivity {
         setContentView(R.layout.admin_manage);
 
         TextView userlist = findViewById(R.id.AdminUserList);
-        Button fire = findViewById(R.id.AdminFire);
+        Button fire = findViewById(R.id.fire_btn);
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
